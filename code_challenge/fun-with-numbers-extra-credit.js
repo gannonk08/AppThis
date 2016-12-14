abe25b9efb2eb378381c8f51b6949f7144934d3f
Kristjan Gannon <gannonk08@gmail.com>
2016-12-13 19:53:44 -0700

    solved extra credit

--- code_challenge/fun-with-numbers-extra-credit.js
+++ code_challenge/fun-with-numbers-extra-credit.js
@@ -1,34 +1,47 @@
 function numberParserEC(arrayOfNums, numOfPiles) {
 
   const hiToLoArray = arrayOfNums.sort((a, b) => {
-    return b-a
+    return a-b
   });
-
-  const numArrayLength = arrayOfNums.length
-  if (numArrayLength / numOfPiles < 1) {
-    return "number larger than digits"
-  }
-  else {
-    var largestPileSize = 1;
-    let pileIndex = 1;
-    while ((numArrayLength-largestPileSize)/numOfPiles > 1) {
-      pileIndex++
-      largestPileSize = pileIndex;
-
-    }
-    return largestPileSize
-
-
+  let arrangedArrays = [];
+  for (var i = 0; i < numOfPiles; i++) {
+    arrangedArrays.push([]);
   }
 
+  while (hiToLoArray.length > 0) {
+    arrangedArrays.forEach((array) => {
+      if (hiToLoArray.length) {
+        array.push(hiToLoArray.pop())
+      }
+    })
+  }
 
+  let trueArray =
+  arrangedArrays.map((array) => {
+    return array.map((number,i) => {
+      let arrayLength = array.length;
+      return number * Math.pow(10, arrayLength - (i + 1));
+    })
+  })
 
+  let oneDimensionArray = trueArray.map((array) => {
+    return array.reduce(function(a, b) {
+      return a + b;
+    }, 0);
+  })
+
+  let total = oneDimensionArray.reduce(function(a, b) {
+    return a + b;
+  }, 0);
+
+  return total;
 }
+
 var testArrayOne = [3, 6, 4, 9, 0, 1]
 var testNumberOne = 4
-var testArrayTwo = [1, 2, 3, 1, 2, 3, 4, 3, 4, 3, 4]
-var testNumberTwo = 4
 
+var testArrayTwo = [1, 2, 3, 1, 2, 3, 4]
+var testNumberTwo = 3
 
 console.log("Extra Credit One is: ", numberParserEC(testArrayOne,testNumberOne));
 console.log("Extra Credit Two is: ", numberParserEC(testArrayTwo,testNumberTwo));
